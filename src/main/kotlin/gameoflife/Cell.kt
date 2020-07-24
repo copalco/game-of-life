948345a2ae0daee04c03cc159a4d5bbaed05f3ca
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-24 22:57:54 +0200

    Make cell constructor private to disallow creating invalid cells

--- src/main/kotlin/gameoflife/Cell.kt
+++ src/main/kotlin/gameoflife/Cell.kt
@@ -1,6 +1,6 @@
 package gameoflife
 
-data class Cell(val state: String) {
+data class Cell private constructor(val state: String) {
     companion object {
         fun dead() = Cell("O")
         fun alive() = Cell("X")
