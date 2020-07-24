0576995bfcd2158f84b2c53e9789297da391ebeb
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-24 22:39:32 +0200

    Pass Cell to the Universe in the next generation

--- src/main/kotlin/gameoflife/Universe.kt
+++ src/main/kotlin/gameoflife/Universe.kt
@@ -4,6 +4,6 @@ data class Universe(private val cell: String) {
     constructor(cell: Cell) : this(cell.state)
 
     fun nextGeneration(): Universe {
-        return Universe("O")
+        return Universe(Cell("O"))
     }
 }
