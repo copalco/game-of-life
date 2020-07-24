6dc219c56a1b9d166f8c6c939c6fb82dcc7293a7
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-24 22:11:23 +0200

    Inline universes in tests

--- src/test/kotlin/UniverseTest.kt
+++ src/test/kotlin/UniverseTest.kt
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test
 
 class UniverseTest {
     @Test fun `dead cell stays dead`() {
-        val universe = Universe("O").nextGeneration()
-        assertThat(universe, equalTo(Universe("O")))
+        assertThat(Universe("O").nextGeneration(), equalTo(Universe("O")))
     }
 
     @Test fun `alive cell dies`() {
-        val universe = Universe("X").nextGeneration()
-        assertThat(universe, equalTo(Universe("O")))
+        assertThat(Universe("X").nextGeneration(), equalTo(Universe("O")))
     }
 }
