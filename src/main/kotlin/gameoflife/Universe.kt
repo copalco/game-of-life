39404da853219c7ac286c309606e0d1fbd0d4b44
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-24 23:38:44 +0200

    Accept multiple cells

--- src/main/kotlin/gameoflife/Universe.kt
+++ src/main/kotlin/gameoflife/Universe.kt
@@ -1,9 +1,7 @@
 package gameoflife
 
 data class Universe(private val cells: List<Cell>) {
-    constructor(cell1: Cell, cell2: Cell) : this(listOf(cell1, cell2))
-
-    constructor(cell: Cell) : this(listOf(cell))
+    constructor(vararg cells: Cell) : this(cells.toList())
 
     fun nextGeneration(): Universe {
         return Universe(cells.map { Cell.dead() })
