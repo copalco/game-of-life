0e0f2412aa8d04a8b186e73a0db77e48ef0f081f
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-24 21:56:43 +0200

    Import assertEquals directly

--- src/test/kotlin/UniverseTest.kt
+++ src/test/kotlin/UniverseTest.kt
@@ -1,10 +1,10 @@
 import gameoflife.Universe
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class UniverseTest {
     @Test fun `dead cell stays dead`() {
         val universe = Universe("O").nextGeneration()
-        Assertions.assertEquals(universe, Universe("O"))
+        assertEquals(universe, Universe("O"))
     }
 }
