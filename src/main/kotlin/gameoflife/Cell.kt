b17bfe0f739213aef7537cec9576bf53f9566c32
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-26 21:20:22 +0200

    Add column to alive cells

--- src/main/kotlin/gameoflife/Cell.kt
+++ src/main/kotlin/gameoflife/Cell.kt
@@ -3,7 +3,7 @@ package gameoflife
 data class Cell private constructor(private val state: State, private val column: Int) {
     companion object {
         fun dead(column: Int = 1) = Cell(State.DEAD, column)
-        fun alive() = Cell(State.ALIVE, column = -1)
+        fun alive(column: Int = 1) = Cell(State.ALIVE, column)
     }
 
     private enum class State { DEAD, ALIVE }
--- src/test/kotlin/CellTest.kt
+++ src/test/kotlin/CellTest.kt
@@ -15,4 +15,12 @@ class CellTest {
     @Test fun `two dead cells with different columns differ`() {
         assertThat(Cell.dead(column = 1), !equalTo(Cell.dead(column = 2)))
     }
+
+    @Test fun `two alive cells with the same column are the same`() {
+        assertThat(Cell.alive(column = 1), equalTo(Cell.alive(column = 1)))
+    }
+
+    @Test fun `two alive cells with different columns differ`() {
+        assertThat(Cell.alive(column = 1), !equalTo(Cell.alive(column = 2)))
+    }
 }
