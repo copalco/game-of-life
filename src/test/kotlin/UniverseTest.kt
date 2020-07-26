592d373f0262ba8e21c8908ac9ea89ad7aaf87ce
Piotr Kopalko <piotr.kopalko@yougov.com>
2020-07-26 21:24:55 +0200

    Pass column to cells in universe tests

--- src/test/kotlin/UniverseTest.kt
+++ src/test/kotlin/UniverseTest.kt
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test
 
 class UniverseTest {
     @Test fun `dead cell stays dead`() {
-        assertThat(Universe(Cell.dead()).nextGeneration(), equalTo(Universe(Cell.dead())))
+        assertThat(Universe(Cell.dead(column = 1)).nextGeneration(), equalTo(Universe(Cell.dead(column = 1))))
     }
 
     @Test fun `alive cell dies`() {
-        assertThat(Universe(Cell.alive()).nextGeneration(), equalTo(Universe(Cell.dead())))
+        assertThat(Universe(Cell.alive(column = 1)).nextGeneration(), equalTo(Universe(Cell.dead(column = 1))))
     }
 
     @Test fun `two dead cells stay dead`() {
         assertThat(
-            Universe(Cell.dead(), Cell.dead()).nextGeneration(),
-            equalTo(Universe(Cell.dead(), Cell.dead()))
+            Universe(Cell.dead(column = 1), Cell.dead(column = 1)).nextGeneration(),
+            equalTo(Universe(Cell.dead(column = 1), Cell.dead(column = 1)))
         )
     }
 }
